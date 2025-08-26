3eb7a3779f10690ad0b0bf812ae5d2463f74dce1
Safir-sudo <safira.da@aluno.edu.pi.gov.br>
2025-08-26 14:02:53 -0300

    Add files via upload
    Primeiro projeto java

---
+++ primeiraAtividade.java
@@ -0,0 +1,8 @@
+public class primeiraAtividade {
+    public static void main(String[] args) {
+            double fahrenheit = 20;
+            double celsius = (5.0 / 9.0) * (fahrenheit -70);
+            System.out.println("Temperatura em Celsius: " + celsius);
+
+    }
+}
